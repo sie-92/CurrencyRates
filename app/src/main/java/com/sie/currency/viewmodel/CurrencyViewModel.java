c42acab6fbd905c21c813309819d1a5d8d5ab11d
sie <ie.sakraoui@gmail.com>
2019-09-08 00:45:57 +0100

    next

--- app/src/main/java/com/sie/currency/viewmodel/CurrencyViewModel.java
+++ app/src/main/java/com/sie/currency/viewmodel/CurrencyViewModel.java
@@ -6,5 +6,5 @@ public class CurrencyViewModel extends ViewModel {
 
     public String curBase = "EUR";
     public double baseValue = 1;
-    public int apiCallPeriod = 8000;
+    public int apiCallPeriod = 1000;
 }
